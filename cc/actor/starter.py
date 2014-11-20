9c26d94cf6b9f4c8863270d045db1272ccd10a8b
Che-Liang Chiou <clchiou@gmail.com>
2014-11-20 00:34:51 -0800

    Add stash/unstash

--- cc/actor/starter.py
+++ cc/actor/starter.py
@@ -7,6 +7,8 @@
 __all__ = ['starter']
 
 import datetime
+import pickle
+import threading
 
 import cc
 import cc.actor
@@ -27,37 +29,83 @@ def init_argparser():
 
     parser = cc.statics.parser
     parser.add_argument(
-        '--start', required=True, type=date,
+        '--start', type=date,
         help='set start date (required, format: YYYY-MM-DD)')
     parser.add_argument(
-        '--end', required=True, type=date,
+        '--end', type=date,
         help='set end date (required, format: YYYY-MM-DD)')
     parser.add_argument(
         '--step', default='1m',
         help='set date step size (default: %(default)s)')
+    parser.add_argument(
+        '--stash',
+        help='store download job info to file')
+    parser.add_argument(
+        '--unstash',
+        help='load download job info from file')
     parser.add_argument(
         'show_url',
         help='the show website, such as http://thecolbertreport.cc.com/')
 
 
 @cc.inits.init
-def init_check_dates():
+def init_check_args():
     parser = cc.statics.parser
     args = cc.statics.args
-    if args.start > args.end:
-        parser.error('start date (%s) is later than end date (%s)' %
-                     (args.start, args.end))
+    if args.stash is not None and args.unstash is not None:
+        parser.error('can only set either --stash or --unstash')
+    if args.start is not None and args.end is not None:
+        if args.unstash is not None:
+            parser.error('cannot set both --unstash and --start/--end')
+        if args.start > args.end:
+            parser.error('start date (%s) is later than end date (%s)' %
+                         (args.start, args.end))
+    else:
+        if args.unstash is None or args.stash is not None:
+            parser.error('need at least --start and --end')
+
+
+@cc.inits.init(cc.inits.Level.LATE)
+def init_stash():
+    args = cc.statics.args
+    if args.stash is not None:
+        cc.statics.pickle_file = open(args.stash, 'wb')
+        cc.statics.pickle_lock = threading.RLock()
+    elif args.unstash is not None:
+        cc.statics.pickle_file = open(args.unstash, 'rb')
+        cc.statics.pickle_lock = threading.RLock()
+
+
+@cc.inits.final
+def final_stash():
+    args = cc.statics.args
+    if args.stash is not None or args.unstash is not None:
+        cc.statics.pickle_file.close()
 
 
 @cc.actor.actor
 def starter():
     '''Start the actor formation!'''
     args = cc.statics.args
-    _starter(args.show_url, args.start, args.end, args.step)
+    _starter(args.show_url,
+             args.unstash is not None,
+             args.start,
+             args.end,
+             args.step)
 
 
-def _starter(show_url, start, end, step):
+def _starter(show_url, is_unstashing, start, end, step):
     logging.info('starter: show_url=%s', show_url)
+    if is_unstashing:
+        with cc.statics.pickle_lock:
+            pickle_file = cc.statics.pickle_file
+            try:
+                while True:
+                    episode = pickle.load(pickle_file)
+                    cc.actor.downloader.downloader(episode)
+            except EOFError:
+                pass
+        return
     feed = cc.feed.Feed.from_show_url(show_url)
     logging.debug('feed...'
                   '\n  url=%s'
@@ -72,6 +120,15 @@ def _starter(show_url, start, end, step):
 @cc.actor.actor
 def starter_helper(sub_feed):
     '''Retrieve episodes and pass them to downloader().'''
+    args = cc.statics.args
+    _starter_helper(sub_feed, args.stash is not None)
+
+
+def _starter_helper(sub_feed, is_stashing):
     logging.info('starter_helper: sub_feed.url=%s', sub_feed.url)
     for episode in cc.episode.Episode.make_episodes(sub_feed):
-        cc.actor.downloader.downloader(episode)
+        if is_stashing:
+            with cc.statics.pickle_lock:
+                pickle.dump(episode, cc.statics.pickle_file)
+        else:
+            cc.actor.downloader.downloader(episode)
